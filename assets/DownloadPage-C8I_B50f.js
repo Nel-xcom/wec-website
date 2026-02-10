bd6cec5649e50d4999207dd7a768f18f3ea83128
Santiago Campanello <terricolaproject@gmail.com>
2026-02-10 11:21:42 -0300

    Updates

--- assets/DownloadPage-C8I_B50f.js
+++ assets/DownloadPage-C8I_B50f.js
@@ -1,4 +1,4 @@
-import{g as l,r as n,j as e,k as d,m as t}from"./index-CRPbSYeq.js";/**
+import{g as l,r as n,j as e,k as d,m as t}from"./index-D_qkwI2a.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
