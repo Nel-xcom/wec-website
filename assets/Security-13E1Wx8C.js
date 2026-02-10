2e81b986a2968df39ffca11c2e9c1003d249b30b
Santiago Campanello <terricolaproject@gmail.com>
2026-02-10 11:52:21 -0300

    Updates

--- assets/Security-13E1Wx8C.js
+++ assets/Security-13E1Wx8C.js
@@ -1,4 +1,4 @@
-import{g as a,r as i,j as e,m as s}from"./index-EyDtBGMU.js";/**
+import{g as a,r as i,j as e,m as s}from"./index-GIzpTafd.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
