4aaa2d30e1bdd1326ca56c6d4f81e8e43820ab2b
Santiago Campanello <terricolaproject@gmail.com>
2026-02-11 10:56:27 -0300

    Updates

--- assets/Intelligence-BQ2K2IOz.js
+++ assets/Intelligence-BQ2K2IOz.js
@@ -1,4 +1,4 @@
-import{h as n,r as x,u as b,j as e,m as l,A as p}from"./index-CujHX70j.js";/**
+import{h as n,r as x,u as b,j as e,m as l,A as p}from"./index-Ci4jrsdg.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
